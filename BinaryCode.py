87f8902910dc04b5e15639833ccc184175ba34c7
Daniel Mogil <danmogil@gmail.com>
2022-04-18 11:24:10 -0400

    expand shell

--- BinaryCode.py
+++ BinaryCode.py
@@ -1,14 +1,16 @@
 class BinaryCode:
-  codewords = []
+  codewords: list[str] = []
+  __fileName: str
   
-  def __init__(self, fileName):
-    f = open(fileName) 
-    self.codewords = f.read().split()
+  def __init__(self, fileName: str):
+    with open(fileName) as f: 
+      self.codewords = f.read().split() #allow variable length codewords?
+      self.__fileName = fileName
   
-  def numDetectableErrors():
+  def numDetectableErrors() -> int:
     pass
 
-  def numCorrectableErrors():
+  def numCorrectableErrors() -> int:
     pass
 
   def detectErrors():
@@ -16,3 +18,15 @@ class BinaryCode:
 
   def correctErrors():
     pass
+
+  def writeCorrected(self):
+    self.correctErrors()
+    with open(f'CORRECTED_{self.__fileName}', 'w') as f:
+      f.write(self.codewords)
+
+  def __getHammingDistance() -> int:
+    pass
+
+  def __getMinDistance() -> int:
+    pass
+
