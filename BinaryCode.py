881e0877d351e506de1e9f3307ca24d6ccd7c75b
Daniel Mogil <danmogil@gmail.com>
2022-04-18 10:21:39 -0400

    class shell

---
+++ BinaryCode.py
@@ -0,0 +1,18 @@
+class BinaryCode:
+  codewords = []
+  
+  def __init__(self, fileName):
+    f = open(fileName) 
+    self.codewords = f.read().split()
+  
+  def numDetectableErrors():
+    pass
+
+  def numCorrectableErrors():
+    pass
+
+  def detectErrors():
+    pass
+
+  def correctErrors():
+    pass
