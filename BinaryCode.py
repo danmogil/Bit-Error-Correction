0920125e82923d81af7a04b948376a1ebe8d71c6
Daniel Mogil <danmogil@gmail.com>
2022-04-19 11:47:19 -0400

    error handling

--- BinaryCode.py
+++ BinaryCode.py
@@ -1,21 +1,22 @@
-import os
+from os import path
 
 class BinaryCode:
   codewords: list[str] = []
   __fileName: str
   __length: int
   
-  def __init__(self, path: str):
-    with open(path) as f: 
+  def __init__(self, file_path: str):
+    with open(file_path) as f: 
       self.codewords = f.read().split()
-      self.__fileName = os.path.basename(path)
+      self.__fileName = path.basename(file_path)
       self.__length = len(self.codewords[0])
   
-  def numDetectableErrors() -> int:
-    pass
+  def numDetectableErrors(self) -> int:
+    minDistance = self.getMinDistance() - 1
+    return minDistance if minDistance > -1 else 0
 
-  def numCorrectableErrors() -> int:
-    pass
+  def numCorrectableErrors(self) -> int:
+    return self.numDetectableErrors() // 2
 
   def detectErrors():
     pass
@@ -29,6 +30,7 @@ class BinaryCode:
       f.write(self.codewords)
 
   def getHammingDistance(self, i, j) -> int:
+    self.__assertDistanceConditions()
     distance = 0
     for i, val in enumerate(self.codewords[i]): 
       if val != self.codewords[j][i]:
@@ -36,6 +38,7 @@ class BinaryCode:
     return distance 
 
   def getMinDistance(self) -> int:
+    self.__assertDistanceConditions()
     min = self.__length
     for i in range(len(self.codewords) - 1):
       for j in range(i + 1, len(self.codewords)):
@@ -45,3 +48,11 @@ class BinaryCode:
         if hammingDistance < min:
           min = hammingDistance
     return min
+
+  def __assertDistanceConditions(self):
+    try:
+      assert len(self.codewords) > 1, "Error: |C| < 2"
+      assert all(len(codeword) == self.__length for codeword in self.codewords), "Error: variable codeword length"
+    except AssertionError as error:
+      print(error)
+      exit()
